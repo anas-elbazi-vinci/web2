888760c772311ae23bcb76578723365d561e56b6
anas-elbazi-vinci <anas.elbazi@student.vinci.be>
2025-09-29 12:18:54 +0200

    new : ex 1.6

---
+++ exercices/1.6/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+import filmRouter from "./routes/films";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/films", filmRouter);
+
+let requestCount = 0;
+app.use((req, _res, next) => {
+  if (req.method === "GET") {
+    requestCount++;
+    console.log(`GET counter : ${requestCount}`);
+  }
+  next();
+});
+
+export default app;
